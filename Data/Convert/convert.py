fc50213a91e82b6920963fa9e778ba96063c000e
Ouhznehc <OuhznehcPersonal@gmail.com>
2023-04-01 21:05:28 +0800

    data

---
+++ Data/Convert/convert.py
@@ -0,0 +1,14 @@
+# kg -> lb
+while True:
+    a = float(input())
+    print("{:.2f}".format(a *  2.20 ))
+
+# # m -> ft
+# while True:
+#     a = float(input())
+#     print("{:.2f}".format(a *  3.28084 ))
+
+# # L -> Gal
+# while True:
+#     a = float(input())
+#     print("{:.2f}".format(a *  0.26417205234375 ))
